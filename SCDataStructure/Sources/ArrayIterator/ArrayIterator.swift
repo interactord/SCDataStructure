a3eccc9da8aad307901efed6a09b74b36c6a55d0
Scott Moon <interactord@gmail.com>
2019-08-13 00:40:12 +0900

    추가:  ArrayIterator ( IteratorProtocol 준수 )

---
+++ SCDataStructure/Sources/ArrayIterator/ArrayIterator.swift
@@ -0,0 +1,21 @@
+//
+// Created by Scott Moon on 2019-08-13.
+// Copyright (c) 2019 Scott Moon. All rights reserved.
+//
+
+import Foundation
+
+public struct ArrayIterator<T>: IteratorProtocol {
+  var currentElement: [T]
+
+  public init(elements: [T]) {
+    self.currentElement = elements
+  }
+
+  public mutating func next() -> T? {
+    return self.currentElement.isEmpty
+      ? nil
+      : self.currentElement.popLast()
+  }
+
+}
