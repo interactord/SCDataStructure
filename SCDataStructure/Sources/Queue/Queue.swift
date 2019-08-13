17ec2f7141fb20a8a4fe01134647621f31352885
Scott Moon <interactord@gmail.com>
2019-08-13 18:15:42 +0900

    추가: 프로토콜 확장
    다음과 같은 기능을 사용하기 위해 확장을 추가하였습니다.
    1.  내부 요소의 디버깅: CustomDebugStringConvertible, CustomStringConvertible 
    2.  반복기(map, filter, for..in 등) 대응 : Sequence:
    3. 내부 요소 변경 및 조회 :  MutableCollection

--- SCDataStructure/Sources/Queue/Queue.swift
+++ SCDataStructure/Sources/Queue/Queue.swift
@@ -11,6 +11,11 @@ public struct Queue<T> {
 
   public init() { }
 
+  /// 시쿼스 타입 이니셜 라이징
+  public init<S: Sequence>(_ element: S) where S.Iterator.Element == T {
+    data.append(contentsOf: element)
+  }
+
   /// 큐에서 첫번째 요소를 제거하고 반환
   /// - 반환값:
   /// - 큐의 요소가 빈 경우, nil
@@ -54,6 +59,65 @@ public struct Queue<T> {
   /// - 비어있지 않는 경우, false 반환
   public var isEmpty: Bool {
     return data.isEmpty
+  }
+
+}
+
+// MARK: - CustomDebugStringConvertible, CustomStringConvertible
+/// 큐의의 요소 String 타입으로 출력하기 위한 프로토콜 구현
+extension Queue: CustomDebugStringConvertible, CustomStringConvertible {
+
+  public var debugDescription: String {
+    return data.debugDescription
+  }
+  public var description: String {
+    return data.description
+  }
+}
+
+// MARK: - ExpressibleByArrayLiteral
+/// List 타입의 배열 초기화를 사용하기 위한 프로토콜 구현
+extension Queue: ExpressibleByArrayLiteral {
+
+  public init(arrayLiteral elements: T...) {
+    self.init(elements)
+  }
+
+}
+
+// MARK: - Sequence
+/// 반복기(Iterator)를 사용하기 위한 프로토콜 구현
+/// 복잡도: O(1)
+extension Queue: Sequence {
+  public func makeIterator() -> AnyIterator<T> {
+    return AnyIterator(
+      IndexingIterator(_elements: data.lazy)
+    )
+  }
+}
+
+// MARK: - MutableCollection
+/// 서버스크립트를 사용하여, 특정 인덱스 값을 조회 또는 변경이 가능하기 위한 프로토콜 구현
+extension Queue: MutableCollection {
+  public var startIndex: Int {
+    return 0
+  }
+
+  public var endIndex: Int {
+    return count - 1
+  }
+
+  public func index(after i: Int) -> Int {
+    return data.index(after: i)
+  }
+
+  public subscript(index: Int) -> T {
+    get {
+      return data[index]
+    }
+    set {
+      data[index] = newValue
+    }
   }
 
 }
--- SCDataStructureTests/Sources/Queue/QueueSpec.swift
+++ SCDataStructureTests/Sources/Queue/QueueSpec.swift
@@ -8,44 +8,44 @@ import SCDataStructure
 
 class QueueSpec: XCTestCase {
 
-	var sut: Queue<Int>!
+  var sut: Queue<Int>!
 
-	override func setUp() {
-		super.setUp()
-		sut = Queue<Int>()
-	}
+  override func setUp() {
+    super.setUp()
+    sut = Queue<Int>()
+  }
 
-	override func tearDown() {
-		sut = nil
-		super.tearDown()
-	}
+  override func tearDown() {
+    sut = nil
+    super.tearDown()
+  }
 
-	func test_empty_element_sut() {
-		XCTAssertTrue(sut.isEmpty)
-		XCTAssertEqual(sut.count, 0)
-		XCTAssertNil(sut.peek)
-		XCTAssertNil(sut.dequeue())
-	}
+  func test_empty_element_sut() {
+    XCTAssertTrue(sut.isEmpty)
+    XCTAssertEqual(sut.count, 0)
+    XCTAssertNil(sut.peek)
+    XCTAssertNil(sut.dequeue())
+  }
 
-	func test_limit_space_sut() {
+  func test_limit_space_sut() {
 
-		sut.enqueue(10)
-		sut.enqueue(20)
+    sut.enqueue(10)
+    sut.enqueue(20)
 
-		XCTAssertFalse(sut.isEmpty)
-		XCTAssertEqual(sut.peek, 10)
-		XCTAssertEqual(sut.dequeue(), 10)
+    XCTAssertFalse(sut.isEmpty)
+    XCTAssertEqual(sut.peek, 10)
+    XCTAssertEqual(sut.dequeue(), 10)
 
-		XCTAssertEqual(sut.peek, 20)
-		XCTAssertEqual(sut.dequeue(), 20)
+    XCTAssertEqual(sut.peek, 20)
+    XCTAssertEqual(sut.dequeue(), 20)
 
-		XCTAssertTrue(sut.isEmpty)
-		XCTAssertEqual(sut.count, 0)
-		XCTAssertNil(sut.peek)
-		XCTAssertNil(sut.dequeue())
-	}
+    XCTAssertTrue(sut.isEmpty)
+    XCTAssertEqual(sut.count, 0)
+    XCTAssertNil(sut.peek)
+    XCTAssertNil(sut.dequeue())
+  }
 
- func test_clear_sut() {
+  func test_clear_sut() {
     sut.enqueue(10)
     sut.enqueue(20)
 
@@ -59,4 +59,101 @@ class QueueSpec: XCTestCase {
     XCTAssertNil(sut.peek)
     XCTAssertNil(sut.dequeue())
   }
+}
+
+// MARK: - CustomDebugStringConvertible, CustomStringConvertible
+extension QueueSpec {
+  func test_empty_element_description() {
+    XCTAssertEqual(sut.debugDescription, "Optional([])")
+    XCTAssertEqual(sut.description, "[]")
+  }
+
+  func test_many_element_description() {
+    sut.enqueue(10)
+    sut.enqueue(20)
+    sut.enqueue(30)
+
+    XCTAssertEqual(sut.debugDescription, "Optional([10, 20, 30])")
+    XCTAssertEqual(sut.description, "[10, 20, 30]")
+  }
+}
+
+// MARK: - ExpressibleByArrayLiteral
+extension QueueSpec {
+  func test_case1_array_init() {
+    let list = Queue(arrayLiteral: 10, 20, 30)
+    XCTAssertFalse(list.isEmpty)
+    XCTAssertEqual(list.count, 3)
+  }
+
+  func test_case2_array_init() {
+    let list: Queue<Int> = [10, 20, 30]
+    XCTAssertFalse(list.isEmpty)
+    XCTAssertEqual(list.count, 3)
+  }
+}
+
+// MARK: - Sequence
+extension QueueSpec {
+  func test_case1_iterator() {
+    var list = Queue<Int>()
+    list.enqueue(10)
+    list.enqueue(20)
+    list.enqueue(30)
+
+    for element in list {
+      sut.enqueue(element)
+    }
+
+    XCTAssertFalse(sut.isEmpty)
+    XCTAssertEqual(sut.count, 3)
+    XCTAssertEqual(sut.peek, 10)
+  }
+
+  func test_case2_iterator() {
+    sut.enqueue(10)
+    sut.enqueue(20)
+    sut.enqueue(30)
+
+    let expectedList = [10, 20, 30]
+    let resultList = sut.compactMap { $0 }
+    XCTAssertEqual(expectedList, resultList)
+
+    XCTAssertFalse(sut.isEmpty)
+    XCTAssertEqual(sut.count, 3)
+    XCTAssertEqual(sut.peek, 10)
+  }
+}
+
+// MARK: - MutableCollection
+extension QueueSpec {
+  func test_case1_mutableCollection() {
+    sut.enqueue(10)
+    sut.enqueue(20)
+    sut.enqueue(30)
+
+    XCTAssertEqual(sut[0], 10)
+    XCTAssertEqual(sut[1], 20)
+    XCTAssertEqual(sut[2], 30)
+
+    XCTAssertEqual(sut.startIndex, 0)
+    XCTAssertEqual(sut.endIndex, 2)
+  }
+
+  func test_case2_mutableCollection() {
+    sut.enqueue(10)
+    sut.enqueue(20)
+    sut.enqueue(30)
+
+    sut[2] = 40
+    XCTAssertNotEqual(30, sut[2])
+
+    var startIndex = sut.startIndex
+
+    while startIndex != sut.endIndex {
+      startIndex = sut.index(after: startIndex)
+    }
+    XCTAssertEqual(startIndex, sut.endIndex)
+  }
+
 }
